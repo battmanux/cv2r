fe1d36711ec1c7085d75053cf174212a45904bb9
Emmanuel BATT <ebatt@groupeseb.com>
2019-06-21 15:20:38 +0000

    sample app for audio

--- inst/examples/sampleApp.r
+++ inst/examples/sampleApp.r
@@ -1,25 +1,47 @@
 library(shiny)
 library(cv2r)
+library(ggplot2)
 
 
 if ( cv2_available() ) {
     ui <- fluidPage(fluidRow(
-        column(4,inputCv2Cam("video")),
-        column(4,cv2Output(outputId = "zoom")),
-        column(4,cv2Output(outputId = "border"))
-    ))
+        column(3,inputCv2Cam("video", auto_send_video = F, audio = T, audio_buff_size = 4096*4)),
+        column(3,plotOutput(outputId = "plot")),
+        column(3,cv2Output(outputId = "zoom")),
+        column(3,cv2Output(outputId = "border"))
+    ),
+    fluidRow(column(12, plotOutput(outputId = "fullplot"))))
     
     server <- function(input, output, session) {
         
+        full_data <- numeric(0)
         
-        observeEvent(input$audio_snap, {
+        observeEvent(input$video_audio, {
             
-            l_x <- base64enc::base64decode(what = input$video_audio)
-            o <- readBin(l_x, "double", size = 4, n = 50000)
-            
-            output$plot <- renderPlotly({
-                ggplotly(ggplot() + geom_line(aes(x=seq_along(o), y = o )))
+            if (is.null(input$video_audio) || nchar(input$video_audio) < 10) {
+                output$plot <- renderPlot(NULL)
+                full_data <<- numeric(0)
+            } else {
+                l_x <- base64enc::base64decode(what = input$video_audio)
+                writeBin(l_x, size = 1,  con = "out.mp3")
+                l_wave <- tuneR::readMP3("out.mp3")
+                
+                l_data_sel <- l_wave@left[1366:length(l_wave@left)]
+                full_data <<- c(full_data, l_data_sel)
+                
+                output$plot <- renderPlot({
+                    ggplot() +
+                        geom_line(aes(x=seq_along(l_data_sel), y = l_data_sel )) + 
+                        theme_minimal()
                 })
+                
+                output$fullplot <- renderPlot({
+                    spectro(tuneR::Wave(left = full_data, bit=16, samp.rate=44100),
+                            flim = c(0, 4000),
+                            FFT_size = 1024  )
+                    })
+                
+            } 
         })
         
         observeEvent(input$snap, {
