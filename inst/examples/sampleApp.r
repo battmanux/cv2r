371423893eef62d514de2adf453cce54c8fbc430
Emmanuel BATT <ebatt@groupeseb.com>
2019-11-02 21:38:04 +0000

    clean sample app

--- inst/examples/sampleApp.r
+++ inst/examples/sampleApp.r
@@ -8,9 +8,8 @@ if ( cv2_available() ) {
         actionButton(inputId = "snap", label = "Take"),
         actionButton(inputId = "snap2", label = "Take2")),
         fluidRow(
-          column(3,inputCv2Cam("video", auto_send_video = F, fps = 5,
-                               auto_send_audio = T, audio = T, 
-                               audio_buff_size = 4096*4)),
+          column(3,inputCv2Cam("capture",
+                               auto_send_audio = T, audio = T)),
           column(3,plotOutput(outputId = "plot")),
           column(3,cv2Output(outputId = "zoom")),
           column(3,cv2Output(outputId = "border"))
@@ -21,55 +20,45 @@ if ( cv2_available() ) {
         
         full_data <- numeric(0)
         
-        observeEvent(input$video_audio, {
+        observeEvent(input$capture_audio, {
+        
+            l_data_sel <- input$capture_audio
             
-            if (is.null(input$video_audio) || nchar(input$video_audio) < 10) {
-                output$plot <- renderPlot(NULL)
-                full_data <<- numeric(0)
-            } else {
-                l_x <- base64enc::base64decode(what = input$video_audio)
-                writeBin(l_x, size = 1,  con = "out.mp3")
-                l_wave <- tuneR::readMP3("out.mp3")
-                
-                l_data_sel <- l_wave@left[1366:length(l_wave@left)]
-                full_data <<- c(full_data, l_data_sel)
-                
-                output$plot <- renderPlot({
-                    ggplot() +
-                        geom_line(aes(x=seq_along(l_data_sel), y = l_data_sel )) + 
-                        theme_minimal()
+            output$plot <- renderPlot({
+                ggplot() +
+                    geom_line(aes(x=seq_along(l_data_sel), y = l_data_sel )) + 
+                    theme_minimal()
+            })
+            
+            output$fullplot <- renderPlot({
+                spectro(tuneR::Wave(left = input$capture_audio, bit=16, samp.rate=44100),
+                        flim = c(0, 4000),
+                        FFT_size = 1024  )
                 })
-                
-                output$fullplot <- renderPlot({
-                    spectro(tuneR::Wave(left = full_data, bit=16, samp.rate=44100),
-                            flim = c(0, 4000),
-                            FFT_size = 1024  )
-                    })
-                
-            } 
+            
         })
         
         observeEvent(input$snap, {
-          inputCv2CamSnap(session, "video")
+          inputCv2CamSnap(session, "capture")
         })
 
         observeEvent(input$snap2, {
-          inputCv2CamSnap(session, "video", top = 100, left = 100, width = 200, height = 100)
+          inputCv2CamSnap(session, "capture", top = 100, left = 100, width = 200, height = 100)
         })
         
                 
         output$zoom <- renderCv2({
-            input$video[5:200,5:200]
+            input$capture[5:200,5:200]
         }, use_svg = T)
         
         output$border <- renderCv2({
-            imshow(mat = cv2r$Canny(input$video, 10L, 50L) )
+            imshow(mat = cv2r$Canny(input$capture, 10L, 50L) )
         })
     }
     
     if (interactive()) {
-      
+      shinyApp(ui, server)
     }
     
 }
-shinyApp(ui, server)
+
